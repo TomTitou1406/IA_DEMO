25d34e83020bfd1cafd03fe948b93b378746ea36
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2025-10-19 17:07:57 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import "@/styles/globals.css";
 import { Metadata } from "next";
 import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
 
-import NavBar from "@/components/NavBar";
-
+import NavBar from "@/logic/NavBar";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
