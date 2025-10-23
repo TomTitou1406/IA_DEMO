b8fa55db907d5a2f6afa297486af4f4fda14053f
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2025-10-23 21:31:13 +0200

    Update ActionsBar.tsx

--- components/ActionsBar.tsx
+++ components/ActionsBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 type Button = {
   label: string;
   onClick: () => void;
@@ -11,7 +13,11 @@ export default function ActionsBar({ buttons }: ActionsBarProps) {
   return (
     <div>
       {buttons.map((btn) => (
-        <button key={btn.label} onClick={btn.onClick} className="mr-2 px-4 py-2 bg-blue-600 text-white rounded">
+        <button
+          key={btn.label}
+          onClick={btn.onClick}
+          className="mr-2 px-4 py-2 bg-blue-600 text-white rounded"
+        >
           {btn.label}
         </button>
       ))}
