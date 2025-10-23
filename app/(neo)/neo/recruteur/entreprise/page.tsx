ca3fdf882b2f58de0548f48f3c7e8ce54d8ac6eb
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2025-10-23 21:09:42 +0200

    Create page.tsx

---
+++ app/(neo)/neo/recruteur/entreprise/page.tsx
@@ -0,0 +1,22 @@
+import AssistantChat from "@/components/AssistantChat";
+import ActionsBar from "@/components/ActionsBar";
+
+export default function RecruteurEntreprise() {
+  return (
+    <section className="w-full max-w-3xl mx-auto flex flex-col gap-8">
+      <AssistantChat
+        avatar="recruteur"
+        // ...pass history, onSend, etc.
+      />
+      <ActionsBar
+        buttons={[
+          { label: "Décrire mon entreprise", onClick: () => {/* ... */} },
+          { label: "Ajouter un visuel", onClick: () => {/* ... */} },
+        ]}
+      />
+      <div>
+        {/* Section métier/page : formulaire, liste, etc. */}
+      </div>
+    </section>
+  );
+}
