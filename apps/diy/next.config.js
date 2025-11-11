3cb6bce48564e0af8b3bb68b2edd35cb029e7a12
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2025-11-11 19:42:09 +0100

    Update next.config.js

--- apps/diy/next.config.js
+++ apps/diy/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
-  // Désactiver le pre-rendering pour les pages système
-  generateBuildId: async () => {
-    return 'diy-build'
-  },
-  // Force dynamic rendering
-  output: 'standalone',
 };
 
 module.exports = nextConfig;
