a7e095a764e5082adf1436db26244cfd15c64a2d
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2025-11-11 14:19:52 +0100

    Update index.ts

--- packages/shared-avatar/src/index.ts
+++ packages/shared-avatar/src/index.ts
@@ -1,7 +1,5 @@
 // Export tous les composants
 export * from './components/InteractiveAvatar';
-export { default as InteractiveBlock } from './components/InteractiveBlock';
-export { default as InteractiveChatBlock } from './components/InteractiveChatBlock';
 export * from './components/AvatarSession/AudioInput';
 export * from './components/AvatarSession/AvatarControls';
 export * from './components/AvatarSession/AvatarVideo';
