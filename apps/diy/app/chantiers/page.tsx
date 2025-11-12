992c75c22bb7dad5d4e42e8aef24c0b3a66ae7c4
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2025-11-12 23:10:05 +0100

    Update page.tsx

--- apps/diy/app/chantiers/page.tsx
+++ apps/diy/app/chantiers/page.tsx
@@ -218,7 +218,7 @@ export default function ChantiersPage() {
                 <div style={{
                   width: '100%',
                   height: '6px',
-                  background: 'var(--gray-light)',
+                  background: 'var(--blue-light)',
                   borderRadius: '10px',
                   overflow: 'hidden',
                   marginBottom: '1rem'
