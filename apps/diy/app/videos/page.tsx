e58343eedc184b2f73067312022d210140a1cffd
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2025-12-16 20:15:41 +0100

    Update page.tsx

--- apps/diy/app/videos/page.tsx
+++ apps/diy/app/videos/page.tsx
@@ -704,7 +704,7 @@ function VideosContent() {
 
   return (
     
-    <div style={{ padding: '2rem', paddingTop: '4rem', maxWidth: '1200px', margin: '0 auto' }}>
+    <div style={{ padding: '2rem', paddingTop: '5rem', maxWidth: '1200px', margin: '0 auto' }}>
       
       {/* Header sticky */}
       <div style={{
