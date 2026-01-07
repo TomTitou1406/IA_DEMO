5108c664eb253c9598a5c5f34f04558e212dc837
CHRISTOPHE FISCHER <cfischer@opteam-ia.com>
2026-01-07 17:15:29 +0100

    Update page.tsx

--- apps/diy/app/travaux/page.tsx
+++ apps/diy/app/travaux/page.tsx
@@ -474,7 +474,7 @@ export default function TravauxSimplesPage() {
   return (
     <>
       <Breadcrumb 
-        currentLevel="travaux"
+        currentLevel="chantiers"
       />
 
       <div style={{ 
